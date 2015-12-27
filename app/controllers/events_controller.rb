c8f56b5e363cedecfa977060fb93bbf65512c447
Phuong Doan <pdoan@atlassian.com>
2015-12-27 18:34:54 +0700

    create event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,11 +15,29 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
+  # GET /venues/new
   def new
-    abc
+    @event = Event.new
   end
 
   def create 
-    abc
+    @event = Event.new(event_params)
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to @event, notice: 'Venue was successfully created.' }
+        format.json { render :show, status: :created, location: @event }
+      else
+        format.html { render :new }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
   end 
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def event_params
+      params.require(:event)
+      .permit(:name, :starts_at, :ends_at, :hero_image_url, :extended_html_description, :category_id, :venue_id)
+    end
 end
