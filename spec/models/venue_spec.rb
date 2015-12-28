e92e0dbcf8eeabf746cb7831c869e8c2cab88e19
Phuong Doan <pdoan@atlassian.com>
2015-12-29 01:17:22 +0700

    venue spec

--- spec/models/venue_spec.rb
+++ spec/models/venue_spec.rb
@@ -1,5 +1,11 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Venue, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Venue, type: :model do
+  describe "validate" do    
+    it { should validate_uniqueness_of(:name) }
+  end
+
+  describe "relationships" do 
+    it { should belong_to :region }
+  end
+end
