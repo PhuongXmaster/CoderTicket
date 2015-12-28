1fa5f342b0e4c9de124840f4fb44e13a96e6e525
Phuong Doan <pdoan@atlassian.com>
2015-12-28 15:12:51 +0700

    event spec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,11 +1,16 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Event, type: :model do
-#   describe "validate" do    
-#     it "name uniqueness" do
-#       event1 = FactoryGirl.create(:event, name: "Test event")
-#       event2 = FactoryGirl.create(:event, name: "Test event")
-#       expect(event2).eq nil
-#     end
-#   end
-# end
+RSpec.describe Event, type: :model do
+  describe "validate" do    
+    it "name uniqueness" do
+      unique_name = "Test event unique name"
+      event1 = FactoryGirl.create(:event, name: unique_name)
+      event2 = FactoryGirl.build(:event, name: unique_name)
+
+      expect(event2).not_to be_valid
+      expect(event2.errors[:name]).to match ["has already been taken"]
+
+      event1.destroy
+    end
+  end
+end
