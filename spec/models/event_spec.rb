3576d750401f5100b4661e0d6af9d1e497f614de
Phuong Doan <pdoan@atlassian.com>
2015-12-28 16:12:42 +0700

    complete event model test

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -5,12 +5,18 @@ RSpec.describe Event, type: :model do
     it { should validate_uniqueness_of(:name) }
     # this is a real fail. Not the problem with shoulda matcher. Not sure what's wrong in the code.
     xit { should validate_uniqueness_of(:name).scoped_to(:venue, :starts_at) }
+
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:extended_html_description) }
     it { should validate_presence_of(:venue) }
     it { should validate_presence_of(:category) }
     it { should validate_presence_of(:starts_at) }
 
+    it { should belong_to :venue }
+    it { should belong_to :category }
+    it { should belong_to(:creator).class_name 'User' }
+
+    it { should have_many :ticket_types }
   end
 
 end
