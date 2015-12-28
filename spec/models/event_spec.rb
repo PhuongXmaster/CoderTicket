7564dbebc164e03b5979f4bb2566b8c3712ae5bf
Phuong Doan <pdoan@atlassian.com>
2015-12-28 15:59:14 +0700

    replace with shoulda matcher for uniqueness

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,33 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Event, type: :model do
   describe "validate" do    
-    before do 
-      unique_name = "Test event unique name"
-      @event1 = FactoryGirl.create(:event, name: unique_name)
-      @event2 = FactoryGirl.build(:event, name: unique_name)
-    end
-
-    after do 
-      @event1.destroy
-    end
-
-    it "name uniqueness" do
-      expect(@event2).not_to be_valid 
-      expect(@event2.errors[:name]).to match ["has already been taken"]
-    end
-
-    # behavior doesn't pass, no idea why
-    xit "can have the same name if different venues" do
-      @event2.venue = FactoryGirl.create(:venue)
-      expect(@event2).to be_valid
-    end
-
-    # behavior doesn't pass, no idea why
-    xit "can have the same name if different start time" do
-      @event2.starts_at = Time.now
-      expect(@event2).to be_valid
-    end
-
+    it { should validate_uniqueness_of(:name) }
+    # this is a real fail. Not the problem with shoulda matcher. Not sure what's wrong in the code.
+    xit { should validate_uniqueness_of(:name).scoped_to(:venue, :starts_at) }
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:extended_html_description) }
     it { should validate_presence_of(:venue) }
